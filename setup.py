1df5f67dfdfe2dcb787dcffc979e132b1a7fc659
Grant Viklund <gviklund@backcode.com>
2013-04-25 19:03:55 -0700

    Updated distributions

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from distutils.core import setup
 
 setup(name='django-stubtools',
-      version='0.3',
+      version='0.4.0',
       author = "Grant Viklund",
       author_email = "gviklund@backcode.com",
-      description = ("A set of tools for Django to help 'stub-out' an app quickly."),
+      description = "A set of tools for Django to help 'stub-out' an app quickly.",
       license = "BSD",
       keywords = "django app",
-      #homepage = "https://github.com/renderbox/django-stubtools",
+      homepage = "http://github.com/backcode/django-stubtools",
       packages=['stubtools', 'stubtools.core', 'stubtools.management', 'stubtools.management.commands', 'stubtools.tests'],
-      #long_description=read('README'),
       classifiers=[
-          "Development Status :: 3 - Alpha",
+          "Development Status :: 4 - Beta",
           "Framework :: Django",
           "Intended Audience :: Developers",
           "License :: OSI Approved :: BSD License",
+      ],
+      install_requires=[
+          "Django >= 1.4.0",
       ],
 )
--- stubtools/exceptions.py
+++ stubtools/exceptions.py
@@ -7,11 +7,11 @@ class NoProjectPathException(Exception):
     	result = []
 
         result.append( "You need to have either a PROJECT_PATH settings variable." )
-        result.append(  "Add the following to your settings.py:\n" )
-        result.append(  "import os.path\n" )
-        result.append(  "PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))" )
-        result.append(  "if '%s/%s' in PROJECT_PATH:" % (project_name, project_name) )
-        result.append(  "    PROJECT_PATH = PROJECT_PATH.replace('%s/%s', '%s')" % (project_name, project_name, project_name) )
+        result.append( "Add the following to your settings.py:\n" )
+        result.append( "import os.path\n" )
+        result.append( "PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))" )
+        result.append( "if '%s/%s' in PROJECT_PATH:" % (project_name, project_name) )
+        result.append( "    PROJECT_PATH = PROJECT_PATH.replace('%s/%s', '%s')" % (project_name, project_name, project_name) )
 
         result.append( message )
 
