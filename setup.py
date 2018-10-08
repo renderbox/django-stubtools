d2697a2dfb731183d86ee18fb4c247a9929325d4
Grant Viklund <grantv@whitemoondreams.com>
2018-10-08 16:16:01 -0700

    Updating templates and setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,47 @@
-from distutils.core import setup
+from os import path
 
-setup(name='django-stubtools',
-      version='0.7.1',
-      author = "Grant Viklund",
-      author_email = "gviklund@backcode.com",
-      description = "A set of tools for Django to help 'stub-out' an app quickly.",
-      license = "BSD",
-      keywords = "django app",
-      home_page = "http://github.com/backcode/django-stubtools",
-      packages=['stubtools', 'stubtools.core', 'stubtools.management', 'stubtools.management.commands', 'stubtools.tests', 'stubtools.templates'],
-      package_data={'stubtools.templates': ['*.txt']},
-      classifiers=[
+from setuptools import setup
+from setuptools import find_packages
+
+PACKAGE_ROOT = path.abspath(path.dirname(__file__))
+
+def load_description(file_path, file_name):
+    description = ''
+    with open(path.join(file_path, file_name), encoding='utf-8') as f:
+        description = f.read()
+    return description
+
+data = {
+    'name': 'django-stubtools',
+    'version': '0.8.0',
+    'description': "A set of tools for Django to help 'stub-out' an project quickly.",
+    'long_description': load_description(PACKAGE_ROOT, 'README.md'),
+    'license': 'BSD',
+    'classifiers': [
           "Development Status :: 4 - Beta",
           "Framework :: Django",
           "Intended Audience :: Developers",
           "License :: OSI Approved :: MIT License",
           "Programming Language :: Python :: 3"
-      ],
-      install_requires=[
-          "Django>=1.9.0",
-      ],
-)
+    ],
+    'keywords': ['django', 'app'],
+    'url': 'http://github.com/renderbox/django-stubtools',
+    'author': 'Grant Viklund',
+    'author_email': 'renderbox@gmail.com',
+    'maintainer': 'Grant Viklund',
+    'maintainer_email': 'renderbox@gmail.com',
+    'packages': ['stubtools', 'stubtools.core', 'stubtools.management', 'stubtools.management.commands', 'stubtools.tests', 'stubtools.templates'],
+    'package_data': {'stubtools.templates': ['*.j2', '*.txt']},
+    'python_requires': '>=2.7.0',
+    'install_requires': ["Django>=2.0.0", "Jinja2==2.10"],
+    'extras_require': {
+        'dev': [],
+        'docs': ["Sphinx==1.7.5", "coverage==4.5.1"],
+        'test': ["pytest==3.6.1", "coverage==4.5.1"],
+    },
+    'entry_points': {
+        'console_scripts': ['mayapm=mayapm.cmd:main'],
+    }
+}
+
+setup(**data)
