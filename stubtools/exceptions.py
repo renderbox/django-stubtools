1c91c5c56ddb808fc991c7a709074ea993e74a32
Grant Viklund <grantv@whitemoondreams.com>
2018-10-10 13:57:39 -0700

    Fixed headers in files

--- stubtools/exceptions.py
+++ stubtools/exceptions.py
@@ -1,3 +1,11 @@
+#--------------------------------------------
+# Copyright 2013-2018, Grant Viklund
+# @Author: Grant Viklund
+# @Date:   2015-10-27 13:59:25
+# @Last Modified by:   Grant Viklund
+# @Last Modified time: 2018-10-10 13:37:22
+#--------------------------------------------
+
 # EXCEPTIONS
 
 class NoProjectPathException(Exception):
---
+++ stubtools/tests/__init__.py
@@ -0,0 +1,7 @@
+#--------------------------------------------
+# Copyright 2013-2018, Grant Viklund
+# @Author: Grant Viklund
+# @Date:   2015-10-27 13:59:25
+# @Last Modified by:   Grant Viklund
+# @Last Modified time: 2018-10-10 13:57:22
+#--------------------------------------------
