46ad77745e422517ac5e9fcc7066f69215dbd3d6
Grant Viklund <grantv@whitemoondreams.com>
2018-10-30 18:53:33 -0700

    Optimized and simplified search functions.

--- stubtools/core/search.py
+++ stubtools/core/search.py
@@ -3,7 +3,7 @@
 # @Author: Grant Viklund
 # @Date:   2018-10-30 18:31:15
 # @Last modified by:   Grant Viklund
-# @Last Modified time: 2018-10-30 18:34:42
+# @Last Modified time: 2018-10-30 18:52:58
 # --------------------------------------------
 
 def get_first_index(lines, regex):
@@ -16,29 +16,28 @@ def get_first_index(lines, regex):
         if check:
             return c       # Return the line index value when found
 
-def get_last_index(lines, regex):
+    return None     # Returns None if not found
+
+def get_last_index(lines, regex, start=0):
+    '''
+    Get the index value of the last item to match the regex pattern.
+    '''
     result = None
 
-    for c, line in enumerate(lines):
+    for c, line in enumerate(lines[start:]):
         check = regex.findall( line )
 
         if check:
-            result = c       # Return the line index value when found
+            result = c + start       # Return the line index value when found
 
     return result
 
 def get_first_and_last_index(lines, regex):
+    '''
+    Done this way to support some optimizations, like starting searches from an index point.
+    '''
 
-    first = None
-    last = first
-
-    for c, line in enumerate(lines):
-        check = regex.findall( line )
-
-        if check:
-            if first == None:
-                first = c
-
-            last = c       # Make note of the line number
+    first = get_first_index(lines, regex)
+    last = get_last_index(lines, regex, start=first)
 
     return (first, last)
