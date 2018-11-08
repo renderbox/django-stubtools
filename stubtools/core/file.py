f8a0034cff640c20c32a7c2535296f6e6a60af95
Grant Viklund <grantv@whitemoondreams.com>
2018-11-08 11:54:30 -0800

    Added file writer to core.

---
+++ stubtools/core/file.py
@@ -0,0 +1,22 @@
+# --------------------------------------------
+# Copyright 2018 Grant Viklund
+# @Author: Grant Viklund
+# @Date:   2018-11-08 11:30:11
+# @Last modified by:   Grant Viklund
+# @Last Modified time: 2018-11-08 11:42:20
+# --------------------------------------------
+
+import os
+
+def write_file(file_path, data, create_path=True):
+    full_path = os.path.abspath(file_path)          # Make it a full path to reduce issues in parsing direcotry from file
+    file_directory = os.path.dirname(full_path)
+
+    # Check the directory exists
+    if not os.path.exists(file_directory):
+        # Create the full path if needed
+        os.makedirs(file_directory)
+
+    FILE = open(full_path, "w")
+    FILE.write(data)
+    FILE.close()
