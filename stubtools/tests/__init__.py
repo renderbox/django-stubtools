eb9c78805d170e22e4c3df895d4823d9f760d4f7
Grant Viklund <grantv@whitemoondreams.com>
2018-11-09 17:53:44 -0800

    Added unittest to test get_import_range()

--- stubtools/tests/__init__.py
+++ stubtools/tests/__init__.py
@@ -1,7 +1,47 @@
-#--------------------------------------------
-# Copyright 2013-2018, Grant Viklund
-# @Author: Grant Viklund
-# @Date:   2015-10-27 13:59:25
-# @Last Modified by:   Grant Viklund
-# @Last Modified time: 2018-10-10 13:57:22
-#--------------------------------------------
+# #--------------------------------------------
+# # Copyright 2013-2018, Grant Viklund
+# # @Author: Grant Viklund
+# # @Date:   2015-10-27 13:59:25
+# # @Last Modified by:   Grant Viklund
+# # @Last Modified time: 2018-11-09 17:27:21
+# #--------------------------------------------
+# from unittest import TestCase
+
+# # from django.test import TestCase
+# from stubtools.core.parse import get_import_line
+
+# class CoreParseGetImportRangeTestCase(TestCase):
+
+#     def setUp(self):
+
+#         self.data_lines = [
+#             "from django.db import models",
+#             "from django.utils.translation import ugettext_lazy as _",
+#             "#2",
+#             "#3",
+#             "class User(models.Model):",
+#             "    name = models.CharField(_('Name'), max_length=300 )",
+#             "#6",
+#             "#7",
+#         ]
+
+#         self.data_lines_missing_import = [
+#             "# COMMENT"
+#             "from django.utils.translation import ugettext_lazy as _",
+#             "#2",
+#             "#3",
+#             "class User(models.Model):",
+#             "    name = models.CharField(_('Name'), max_length=300 )",
+#             "#6",
+#             "#7",
+#         ]
+
+#     def test_empty_data_results_in_zero(self):
+#         self.assertEqual( get_import_line("^from django.db import models", []), (0,0) )
+
+
+#     # def test_empty_data_results_in_zero(self):
+#     #     self.assertEqual( get_import_line("^from django.db import models", self.data_lines), (0,0) )
+
+#         # self.assertEqual(lion.speak(), 'The lion says "roar"')
+#         # self.assertEqual(cat.speak(), 'The cat says "meow"')
