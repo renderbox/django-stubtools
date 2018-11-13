20d7892f5fac8936fd710ff1cbac41f3c0ebc9ed
Grant Viklund <grantv@whitemoondreams.com>
2018-11-13 12:11:52 -0800

    Working on base slicing the model python file.

--- stubtools/management/commands/stubmodel.py
+++ stubtools/management/commands/stubmodel.py
@@ -3,29 +3,30 @@
 # @Author: Grant Viklund
 # @Date:   2017-02-20 13:50:51
 # @Last Modified by:   Grant Viklund
-# @Last Modified time: 2018-11-09 17:58:33
+# @Last Modified time: 2018-11-13 12:10:13
 #--------------------------------------------
 
 import re, os.path
 # import inspect
 import pprint
 
-from jinja2 import Environment, PackageLoader, select_autoescape
+# from jinja2 import Environment, PackageLoader, select_autoescape
+from django.template.loader import get_template
 
-from django.core.management.base import AppCommand, CommandError
+from django.core.management.base import CommandError
 from django.db import models
 from django.db.models import Model, Field
 
-from stubtools.core import underscore_camel_case, import_line_check, class_name, get_all_subclasses, parse_app_input
+from stubtools.core import FileAppCommand, underscore_camel_case, import_line_check, class_name, get_all_subclasses, parse_app_input, get_file_lines, split_camel_case
 from stubtools.core.prompt import ask_question, ask_yes_no_question, selection_list, horizontal_rule
-from stubtools.core.parse import get_import_range
+from stubtools.core.parse import get_import_range, get_classes_and_functions_start
 
 # Fields that are not really meant to be included in the list of useable Model Fields
 EXCLUDED_FIELDS = ['BLANK_CHOICE_DASH', 'Empty', 'Field', 'FieldDoesNotExist', 'NOT_PROVIDED']
 
 
 
-class Command(AppCommand):
+class Command(FileAppCommand):
     args = '<app.model_name>'
     help = 'creates stub Templates, Forms and Admin entries for a given model name'
     import_line_regex = re.compile(r"^from django.db import (.+)", re.MULTILINE)
@@ -57,9 +58,32 @@ class Command(AppCommand):
         # modelclass_settings.update(MODELCLASS_SETTINGS)     # Update the setting dict with defaults
         model_key = selection_list(model_classes, as_string=True, title="Select a Model Class")
 
+        if not model:
+            default = "MyModel"
+            model = input("What is the name of the model? [%s] > " % default) or default
 
+        # MAKE SURE AT LEAST THE FIRST LETTER OF THE VIEW NAME IS CAPITALIZED
+        model = model[0].upper() + model[1:]
 
-        render_ctx = {'model_key':model_key}
+        model_name = "_".join(split_camel_case(model)).lower()
+
+        render_ctx = {'app':app, 'model_key':model_key, 'model':model, 'model_name':model_name, 'attributes':[],
+                        'model_header':"", 'model_import_statement':"", 'model_pre_model':"", 'model_footer':"" }
+
+        # if not model_class:?
+
+        # Need to replace this with a settings file
+        if render_ctx['model_key'] == "django.db.models.Model":
+            render_ctx['model_class_module'] = "django.db"
+            render_ctx['model_class'] = "models.Model"
+            render_ctx['model_class_import'] = "models"
+        else:
+            render_ctx['model_class_module'] = ".".join( render_ctx['model_key'].split(".")[:-1] )
+            render_ctx['model_class'] = render_ctx['model_key'].split(".")[-1]
+            render_ctx['model_class_import'] = render_ctx['model_class']
+
+        if not render_ctx['attributes']:
+            render_ctx['attributes'] = [{'field_name':'name', 'field_type':"models.CharField", 'field_kwargs':{ 'blank':False }}]
 
         return render_ctx
 
@@ -70,18 +94,10 @@ class Command(AppCommand):
         app, model, model_class = parse_app_input(app_model)
 
         model_file = os.path.join(app, "models.py")
-        # form_file = os.path.join(app, "forms.py")
 
         print("MODEL FILE: %s" % model_file)
 
         render_ctx = self.get_context(app, model, model_class)
-
-        if render_ctx['model_key'] == "django.db.models.Model"
-            render_ctx['model_class_module'] = "django.db"
-            render_ctx['model_class'] = "models.Model"
-        else:
-            render_ctx['model_class_module'] = ".".join( render_ctx['model_key'].split(".")[:-1] )
-            render_ctx['model_class'] = render_ctx['model_key'].split(".")[-1]
 
         # #######################
         # # PARSE models.py
@@ -98,17 +114,36 @@ class Command(AppCommand):
         class_func_end = line_count
 
         # Segment Values
-        model_header = None         # In between the first line and the module import
-        model_pre_model = None      # between the import line and where the model needs to be added
-        model_footer = None         # after the model code
+        # model_header = None         # In between the first line and the module import
+        # model_pre_model = None      # between the import line and where the model needs to be added
+        # model_footer = None         # after the model code
 
         modules = []
         comments = ""
 
         import_start_index, import_end_index = get_import_range("^from %(model_class_module)s import (.+)" % render_ctx, data_lines[:class_func_start])
 
-        if import_start_index > import_end_index:
+        if import_start_index != import_end_index:  # If they are not the same, the import line already exists in the file
             modules, comments = get_classes_and_functions(data_lines[import_start_index])
+
+        modules.append(render_ctx['model_class_import'])
+
+        render_ctx['model_import_statement'] = "from %s import %s" % (render_ctx['model_class_module'], ', '.join(modules) )
+
+        if comments:
+            render_ctx['model_import_statement'] += "# " + comments
+
+        if import_start_index > 0:
+            render_ctx['model_header'] = "".join(data_lines[:url_import_line])
+        else:
+            render_ctx['model_header'] = ""
+
+        # Parts to parse and add to the context
+        # render_ctx['model_header']
+        # render_ctx['model_import_statement']
+        # {{ model_pre_model }}
+        # # MODEL STUFF
+        # {{ model_footer }}
 
         #######################
         # RENDER THE TEMPLATES
@@ -119,6 +154,11 @@ class Command(AppCommand):
         pp.pprint(render_ctx)
         print( horizontal_rule() )
 
+        model_template = get_template('stubtools/stubmodel/model.py.j2', using='jinja2')
+        model_result = model_template.render(context=render_ctx)
+
+        print("models.py RESULT:")
+        print(model_result)
 
         # # LOAD FILE
         # if os.path.isfile( model_file ):
@@ -183,5 +223,5 @@ class Command(AppCommand):
         # mf.close()
 
         # model_results = "\n".join(new_lines)
-        # write_file(model_file, model_results)
+        # self.write_file(model_file, model_results)
 
