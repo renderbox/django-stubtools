a1563d19c9f0935be9b62bb2d5cdd52c389530a9
Grant Viklund <grantv@whitemoondreams.com>
2018-12-14 18:09:23 -0800

    Passing all file_test.py tests

--- stubtools/tests/file_test.py
+++ stubtools/tests/file_test.py
@@ -3,7 +3,7 @@
 # @Author: Grant Viklund
 # @Date:   2015-10-27 13:59:25
 # @Last Modified by:   Grant Viklund
-# @Last Modified time: 2018-12-14 17:54:07
+# @Last Modified time: 2018-12-14 18:08:45
 #--------------------------------------------
 import os
 import pprint
@@ -52,7 +52,7 @@ class PythonFileParserTestCase(TestCase):
         self.assertEqual( import_block, None )
 
 
-    def test_empty_file_header(self):
+    def test_models_empty_header(self):
         file_path = os.path.join(self.test_file_dir, 'models_empty.py')
         parser = PythonFileParser( file_path )
 
@@ -60,7 +60,7 @@ class PythonFileParserTestCase(TestCase):
 
         self.assertEqual( header_array[-1], "# --------------------------------------------" )
 
-    def test_empty_file_body(self):
+    def test_models_empty_body(self):
         file_path = os.path.join(self.test_file_dir, 'models_empty.py')
         parser = PythonFileParser( file_path )
 
@@ -68,7 +68,7 @@ class PythonFileParserTestCase(TestCase):
 
         self.assertEqual( body, "" )
 
-    def test_empty_file_import_block(self):
+    def test_models_empty_import_block(self):
         file_path = os.path.join(self.test_file_dir, 'models_empty.py')
         parser = PythonFileParser( file_path )
 
@@ -139,70 +139,120 @@ class PythonFileParserTestCase(TestCase):
         import_array = parser.get_import_block().splitlines()
 
         self.assertEqual( import_array[0], "from django.db import models" )
-        self.assertEqual( import_array[-1], "from django.utils.translation import ugettext_lazy as _" )
+        self.assertEqual( import_array[-1], "from django.utils.translation import ugettext_lazy as _     # For localization" )
 
 
 
-    # def test_empty_admin_file(self):
-    #     file_path = os.path.join(self.test_file_dir, 'admin_empty.py')
-    #     parser = PythonFileParser( file_path )
-
-    #     self.assertEqual( parser.structure['first_import_line'], 8 )
-    #     self.assertEqual( parser.structure['last_import_line'], 8 )
-    #     self.assertEqual( parser.structure['last_code_line'], None )
-
-    #     # Change Slice Point
-    #     parser.set_import_slice(".models")
-
-    #     self.assertEqual( parser.structure['header_end_index'], 8 )
-    #     self.assertEqual( parser.structure['body_start_index'], None )
-    #     self.assertEqual( parser.structure['body_end_index'], None )
-    #     self.assertEqual( parser.structure['footer_start_index'], None )
-
-    # def test_multiple_admin_file(self):
-    #     file_path = os.path.join(self.test_file_dir, 'admin_multiple.py')
-    #     parser = PythonFileParser( file_path )
-
-    #     self.assertEqual( parser.structure['first_import_line'], 8 )
-    #     self.assertEqual( parser.structure['last_import_line'], 9 )
-    #     self.assertEqual( parser.structure['last_code_line'], 44 )
-
-    #     # Change Slice Point
-    #     parser.set_import_slice(".models")
-
-    #     self.assertEqual( parser.structure['header_end_index'], 7 )
-    #     self.assertEqual( parser.structure['body_start_index'], 9 )
-    #     self.assertEqual( parser.structure['body_end_index'], 43 )
-    #     self.assertEqual( parser.structure['footer_start_index'], 44 )
-
-
-    # def test_multiple_urls_file(self):
-    #     file_path = os.path.join(self.test_file_dir, 'urls_multiple.py')
-    #     parser = PythonFileParser( file_path )
-
-    #     self.assertEqual( parser.structure['first_import_line'], 8 )
-    #     self.assertEqual( parser.structure['last_import_line'], 10 )
-    #     self.assertEqual( parser.structure['last_code_line'], 14 )      # This should be line 15 but the ']' is on line 15 and is not recognized in AST.  A new method to figure this out needs to be developed.
-
-    #     # # Change Slice Point
-    #     # parser.set_import_slice(".models")
-
-    #     # self.assertEqual( parser.structure['header_end_index'], 7 )
-    #     self.assertEqual( parser.structure['body_start_index'], 11 )
-    #     # self.assertEqual( parser.structure['body_end_index'], 43 )
-    #     # self.assertEqual( parser.structure['footer_start_index'], 44 )
-
-
-    def test_registry_api_views_header(self):
-        file_path = os.path.join(self.test_file_dir, 'registry_api_views.py')
+    def test_admin_empty_header(self):
+        file_path = os.path.join(self.test_file_dir, 'admin_empty.py')
         parser = PythonFileParser( file_path )
 
         header_array = parser.new_get_header().splitlines()
 
         self.assertEqual( header_array[-1], "# --------------------------------------------" )
 
-    def test_registry_api_views_body(self):
-        file_path = os.path.join(self.test_file_dir, 'registry_api_views.py')
+    def test_adminempty_body(self):
+        file_path = os.path.join(self.test_file_dir, 'admin_empty.py')
+        parser = PythonFileParser( file_path )
+
+        body = parser.new_get_body()
+
+        self.assertEqual( body, "" )
+
+    def test_admin_empty_import_block(self):
+        file_path = os.path.join(self.test_file_dir, 'admin_empty.py')
+        parser = PythonFileParser( file_path )
+
+        self.assertEqual( parser.structure['first_import_line'], 8 )
+        self.assertEqual( parser.structure['last_import_line'], 8 )
+
+        import_array = parser.get_import_block().splitlines()
+
+        self.assertEqual( import_array[0], "from django.contrib import admin" )
+        self.assertEqual( import_array[-1], "from django.contrib import admin" )
+
+
+
+
+
+    def test_admin_multiple_header(self):
+        file_path = os.path.join(self.test_file_dir, 'admin_multiple.py')
+        parser = PythonFileParser( file_path )
+
+        header_array = parser.new_get_header().splitlines()
+
+        self.assertEqual( header_array[-1], "# --------------------------------------------" )
+
+    def test_admin_multiple_body(self):
+        file_path = os.path.join(self.test_file_dir, 'admin_multiple.py')
+        parser = PythonFileParser( file_path )
+
+        body_array = parser.new_get_body().splitlines()
+
+        self.assertEqual( body_array[0], "" )
+        self.assertEqual( body_array[-1], 'admin.site.register(Product, ProductAdmin)' )
+
+    def test_admin_multiple_import_block(self):
+        file_path = os.path.join(self.test_file_dir, 'admin_multiple.py')
+        parser = PythonFileParser( file_path )
+
+        self.assertEqual( parser.structure['first_import_line'], 8 )
+        self.assertEqual( parser.structure['last_import_line'], 9 )
+
+        import_array = parser.get_import_block().splitlines()
+
+        self.assertEqual( import_array[0], "from django.contrib import admin" )
+        self.assertEqual( import_array[-1], "from .models import Content, Product, Category" )
+
+
+    # Add test for slicing before and after registration...
+
+
+
+    def test_urls_multiple_header(self):
+        file_path = os.path.join(self.test_file_dir, 'urls_multiple.py')
+        parser = PythonFileParser( file_path )
+
+        header_array = parser.new_get_header().splitlines()
+
+        self.assertEqual( header_array[-1], "# --------------------------------------------" )
+
+    def test_urls_multiple_body(self):
+        file_path = os.path.join(self.test_file_dir, 'urls_multiple.py')
+        parser = PythonFileParser( file_path )
+
+        body_array = parser.new_get_body().splitlines()
+
+        self.assertEqual( body_array[0], "" )
+        self.assertEqual( body_array[-1], ']' )
+
+    def test_urls_multiple_import_block(self):
+        file_path = os.path.join(self.test_file_dir, 'urls_multiple.py')
+        parser = PythonFileParser( file_path )
+
+        self.assertEqual( parser.structure['first_import_line'], 8 )
+        self.assertEqual( parser.structure['last_import_line'], 10 )
+
+        import_array = parser.get_import_block().splitlines()
+
+        self.assertEqual( import_array[0], "from django.conf.urls import url" )
+        self.assertEqual( import_array[-1], "from . import views" )
+
+
+    # Add test for slicing before and after url list insert point...
+
+
+
+    def test_api_views_header(self):
+        file_path = os.path.join(self.test_file_dir, 'api_views.py')
+        parser = PythonFileParser( file_path )
+
+        header_array = parser.new_get_header().splitlines()
+
+        self.assertEqual( header_array[-1], "# --------------------------------------------" )
+
+    def test_api_views_body(self):
+        file_path = os.path.join(self.test_file_dir, 'api_views.py')
         parser = PythonFileParser( file_path )
 
         body_array = parser.new_get_body().splitlines()
@@ -210,8 +260,8 @@ class PythonFileParserTestCase(TestCase):
         self.assertEqual( body_array[0], "" )
         self.assertEqual( body_array[-1], "    serializer_class = ReleaseSerializer" )
 
-    def test_registry_api_views_import_block(self):
-        file_path = os.path.join(self.test_file_dir, 'registry_api_views.py')
+    def test_api_views_import_block(self):
+        file_path = os.path.join(self.test_file_dir, 'api_views.py')
         parser = PythonFileParser( file_path )
 
         self.assertEqual( parser.structure['first_import_line'], 8 )
